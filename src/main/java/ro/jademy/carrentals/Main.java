2b3431350ea05f5d6596b48f10fcfd0c5b1a0bc0
Nadina <nadina.florea@orange.com>
2020-08-10 15:20:04 +0300

    Create data and instantiate Shop.

--- src/main/java/ro/jademy/carrentals/Main.java
+++ src/main/java/ro/jademy/carrentals/Main.java
@@ -1,7 +1,15 @@
 package ro.jademy.carrentals;
 
+import ro.jademy.carrentals.data.DataSource;
+import ro.jademy.carrentals.model.Shop;
+
 public class Main {
     public static void main(String[] args) {
 
+        DataSource.createCarList();
+        DataSource.createUserList();
+
+        Shop shop = new Shop();
+        shop.loginScreen();
     }
 }
