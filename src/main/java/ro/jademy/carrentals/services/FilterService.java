ef902c7a47521d25e0340321b209d0c5bdc999a8
Nadina <nadina.florea@orange.com>
2020-08-10 15:45:08 +0300

    Added Filter Service.

---
+++ src/main/java/ro/jademy/carrentals/services/FilterService.java
@@ -0,0 +1,8 @@
+package ro.jademy.carrentals.services;
+
+public interface FilterService {
+    void filterCarsByMake();
+    void filterCarsByModel();
+    void filterCarsByBudget();
+
+}
---
+++ src/main/java/ro/jademy/carrentals/services/FilterServiceImpl.java
@@ -0,0 +1,18 @@
+package ro.jademy.carrentals.services;
+
+public class FilterServiceImpl implements FilterService {
+    @Override
+    public void filterCarsByMake() {
+        //TODO: implement method
+    }
+
+    @Override
+    public void filterCarsByModel() {
+        //TODO: implement method
+    }
+
+    @Override
+    public void filterCarsByBudget() {
+        //TODO: implement method
+    }
+}
