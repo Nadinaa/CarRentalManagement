4e215cecd67ae430d57bab03eeed71e387dbca45
Nadina <nadina.florea@orange.com>
2020-08-10 15:14:01 +0300

    Added constructor and toString method in Car class.

--- src/main/java/ro/jademy/carrentals/model/Car.java
+++ src/main/java/ro/jademy/carrentals/model/Car.java
@@ -20,11 +20,41 @@ public class Car {
     public String transmissionType; // automatic, manual
 
     // Q: how can we better represent the engine?
-    public String engine;
+    public double engine;
 
     // Q: how can we better represent money value?
-    public String basePrice;
+    public long basePrice;
 
     // Q: do we need a constructor other than the default one?
     // Q: how can we better protect the car data?
+
+
+    public Car(String make, String model, Integer year, String carType, String fuelType, Integer doorNumber, String color, String transmissionType, double engine, long basePrice) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.carType = carType;
+        this.fuelType = fuelType;
+        this.doorNumber = doorNumber;
+        this.color = color;
+        this.transmissionType = transmissionType;
+        this.engine = engine;
+        this.basePrice = basePrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", carType='" + carType + '\'' +
+                ", fuelType='" + fuelType + '\'' +
+                ", doorNumber=" + doorNumber +
+                ", color='" + color + '\'' +
+                ", transmissionType='" + transmissionType + '\'' +
+                ", engine=" + engine +
+                ", basePrice=" + basePrice +
+                '}';
+    }
 }
