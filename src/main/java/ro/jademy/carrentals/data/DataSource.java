48ca1e94f7c557f0664c5f96e47f6ab74af7358a
Nadina <nadina.florea@orange.com>
2020-08-12 17:03:08 +0300

    Added new data and modified rental price;

--- src/main/java/ro/jademy/carrentals/data/DataSource.java
+++ src/main/java/ro/jademy/carrentals/data/DataSource.java
@@ -1,15 +1,15 @@
 package ro.jademy.carrentals.data;
 
-import ro.jademy.carrentals.model.Car;
-import ro.jademy.carrentals.model.User;
+import ro.jademy.carrentals.model.cars.Car;
+import ro.jademy.carrentals.model.users.User;
 import ro.jademy.carrentals.model.cars.audiCars.A6;
 import ro.jademy.carrentals.model.cars.audiCars.A7;
 import ro.jademy.carrentals.model.cars.audiCars.A8;
 import ro.jademy.carrentals.model.cars.bmwCars.Series3;
 import ro.jademy.carrentals.model.cars.bmwCars.Series4;
 import ro.jademy.carrentals.model.cars.bmwCars.Series5;
-import ro.jademy.carrentals.users.Customer;
-import ro.jademy.carrentals.users.Salesman;
+import ro.jademy.carrentals.model.users.Customer;
+import ro.jademy.carrentals.model.users.Salesman;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,12 +22,13 @@ public class DataSource {
     public static List<User> userList = new ArrayList<>();
 
     public static void createCarList(){
-        carList.add(new A6(2010, "sedan", "diesel", 4, "black", "manual", 2.2, 22000));
-        carList.add(new A7(2014, "hatchback", "gasoline", 4, "red", "automatic", 2.7, 32000));
-        carList.add(new A8(2019, "coupe", "alternative", 2, "white", "manual", 4.0, 80000));
-        carList.add(new Series3(2010, "coupe", "gasoline", 4, "black", "manual", 2.2, 42000));
-        carList.add(new Series4(2016, "sedan", "diesel", 4, "blue", "automatic", 2.2, 61000));
-        carList.add(new Series5(2019, "sedan", "gasoline", 2, "grey", "automatic", 2.2, 75000));
+        carList.add(new A6(2010, "sedan", "diesel", 4, "black", "manual", 2.2, 220));
+        carList.add(new A7(2014, "hatchback", "gasoline", 4, "red", "automatic", 2.7, 320));
+        carList.add(new A7(2015, "hatchback", "diesel", 4, "white", "manual", 2.5, 330));
+        carList.add(new A8(2019, "coupe", "alternative", 2, "white", "manual", 4.0, 800));
+        carList.add(new Series3(2010, "coupe", "gasoline", 4, "black", "manual", 2.2, 420));
+        carList.add(new Series4(2016, "sedan", "diesel", 4, "blue", "automatic", 2.2, 610));
+        carList.add(new Series5(2019, "sedan", "gasoline", 2, "grey", "automatic", 2.2, 750));
 
         availableCarList = new ArrayList<>(carList);
     }
